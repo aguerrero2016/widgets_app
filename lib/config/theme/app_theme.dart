710c947adeac6318207308d2ce67bf9d0c204b86
Arturo Guerrero <arturo.guerrero.g@gmail.com>
2023-07-08 23:12:32 -0500

    antes de state notifier

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -14,15 +14,18 @@ const colorList = <Color>[
 
 class AppTheme {
   final int selectedColor;
+  final bool isDarkmode;
 
   AppTheme({
     this.selectedColor = 0,
+    this.isDarkmode = false,
   })  : assert(selectedColor >= 0, 'El color debe ser mayor a 0'),
         assert(selectedColor < colorList.length,
             'El color debe ser menor o igual a ${(colorList.length - 1)}}');
 
   ThemeData getTheme() => ThemeData(
       useMaterial3: true,
+      brightness: isDarkmode ? Brightness.dark: Brightness.light,
       colorSchemeSeed: colorList[selectedColor],
       appBarTheme: const AppBarTheme(
         centerTitle: false,
